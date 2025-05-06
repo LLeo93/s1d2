037eb341c86ad03db3be2ccfb82354c54245af0a
LLeo93 <liba.leoncini@gmail.com>
2025-05-06 16:58:02 +0200

    extra sull'anno bisestile

--- es1.java
+++ es1.java
@@ -1,23 +1,44 @@
 package s1d2;
 import java.util.Scanner;
+
 public class es1 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("inserisci una parola");
-        String a = scanner.nextLine();
 
-        if (a.length() % 2 == 0) {
-            System.out.println("la lunghezza è pari");
-        } else {
-            System.out.println("la lunghezza è dispari");
-        }
-        System.out.println("inserisci un anno");
-        int b = scanner.nextInt();
-        if ((b % 4 == 0 && b % 100 != 0) || (b % 400 == 0)) {
-            System.out.println("anno bisestile");
-        } else {
-            System.out.println("anno non bisestile");
 
+//        System.out.println("inserisci una parola");
+//        String a = scanner.nextLine();
+//
+//
+//        if (a.length() % 2 == 0) {
+//            System.out.println("la lunghezza è pari");
+//        } else {
+//            System.out.println("la lunghezza è dispari");
+//        }
+
+
+        while (true) {
+
+            System.out.println("Inserisci un anno (inserisci 0 per uscire):");
+            int b = scanner.nextInt();
+
+
+            if (b == 0) {
+                System.out.println("Uscita dal programma.");
+                break;
+            }
+
+
+            if ((b % 4 == 0 && b % 100 != 0) || (b % 400 == 0)) {
+                System.out.println("Anno bisestile");
+            } else {
+                System.out.println("Anno non bisestile");
+            }
         }
+
+
+        scanner.close();
     }
 }
+
